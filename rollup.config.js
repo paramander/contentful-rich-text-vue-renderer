4c643bed04163e8f4e5cb52b0deed572bb0800b6
Tolga Paksoy <tolga@paramander.com>
2019-04-16 13:17:43 +0200

    Use overrides

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 
-export default {
+export default (outputFile, overrides = {}) => ({
     input: 'src/index.js', // Path relative to package.json
-    output: {
-        name: 'ContentfulRichText',
+    output: [{
+        file: outputFile,
         exports: 'named',
-    },
+        name: 'ContentfulRichText',
+    }],
     plugins: [
         buble({ objectAssign: 'Object.assign' }), // Transpile to ES5
     ],
-};
+    ...overrides,
+});
