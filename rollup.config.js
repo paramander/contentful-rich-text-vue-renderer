b6504f7f0432969ed7652b4db52186ca1ebee0a1
Tolga Paksoy <tolga@paramander.com>
2022-05-26 19:12:50 +0200

    Fix es module build by upgrading rollup script

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,42 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
-import babel from 'rollup-plugin-babel';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+import common from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
 
 export default [
   // browser-friendly UMD build
   {
-    input: 'src/index.js',
-    output: {
-      name: 'ContentfulRichText',
-      file: pkg.browser,
-      format: 'umd',
-    },
+    input: "src/index.js",
+    output: [
+      {
+        name: "ContentfulRichText",
+        file: pkg.browser,
+        format: "umd",
+      },
+    ],
     plugins: [
-      resolve(),
-      commonjs(),
+      nodeResolve({
+        browser: true,
+      }),
+      common(),
       babel({
-        exclude: 'node_modules/**',
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
       }),
     ],
   },
 
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
-    input: 'src/index.js',
+    external: ["@contentful/rich-text-types"],
+    input: "src/index.js",
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "default",
+      },
+      { file: pkg.module, format: "esm" },
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-    ]
+    plugins: [nodeResolve()],
   },
 ];
